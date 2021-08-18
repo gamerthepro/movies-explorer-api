b1b6c2f8888a8308ae47922ce590444b86f735b8
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-08-18 16:13:09 +0300

    fix app.js

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cors = require('cors');
-const users = require('./routes/users');
-const cards = require('./routes/cards');
-const NotFoundError = require('./errors/404-NotFoundError');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { signinValidation, signupValidation } = require('./middlewares/validation');
+require('dotenv').config();
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const routes = require('./routes');
+const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 
@@ -23,29 +21,22 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.disable('x-powered-by');
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+app.use(routes);
 
 app.use(requestLogger);
-
-app.post('/signup', signupValidation, createUser);
-app.post('/signin', signinValidation, login);
-
-app.use('/users', auth, users);
-app.use('/cards', auth, cards);
-
-app.use(errorLogger);
 
 app.use(errors());
 
 app.use(() => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
+
+app.use(errorLogger);
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
@@ -54,6 +45,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`server started, Port: ${PORT}`);
 });
