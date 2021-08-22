9d574811e845899d359526bcee04e63c2e88148a
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-08-22 14:43:08 +0300

    Для имени базы данных используется moviesdb

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
-const { MONGO_DB = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const { MONGO_DB = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
 module.exports = MONGO_DB;
