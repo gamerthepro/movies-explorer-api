8ee975179733c1a6373b5b503a307555b702727a
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-08-22 14:57:20 +0300

    limiter подключен перед логгером запросов.

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(limiter);
 app.use(cookieParser());
 app.use(cors());
+app.use(requestLogger);
+app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,9 +34,6 @@ mongoose.connect(MONGO_DB, {
 });
 
 app.use(routes);
-
-app.use(requestLogger);
-
 app.use(errors());
 
 app.use(() => {
